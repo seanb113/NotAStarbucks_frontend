1c8aaa6210524ec470692d7c09ce0da59be96a9d
Sean Beach <sean.beach1@yahoo.com>
2020-02-28 17:34:07 -0500

    first frontend commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import NavBar from './components/NavBar'
+import CoffeeList from './containers/CoffeeList'
 import './App.css';
 
-function App() {
+class App extends Component {
+  // state = {
+  //   coffee_shops: []
+  // }
+
+  // componentDidMount(){
+  //   fetch("http://localhost:3001/coffee_shops")
+  //   .then(r=>r.json())
+  //   .then(r=>console.log(r))
+  // }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="ui top attached tabular menu">
+    <a class="active item">
+    </a>
+    NotAStarBucks
+    <NavBar/>
+    <CoffeeList/>
     </div>
-  );
+  
+  )
+  }
 }
 
 export default App;
