568a5f4a973be05927a8275335ab9738b3deb70c
Sean Beach <sean.beach1@yahoo.com>
2020-03-03 12:19:45 -0500

    routes fixed

--- src/components/SortControl.js
+++ src/components/SortControl.js
@@ -1,18 +1,27 @@
-import React from 'react'
-const SortControl = (props) => {
-    return (
-        <div className="filterWrapper">
+import React, { Component } from 'react'
+import Slider from 'react-rangeslider'
+import 'react-rangeslider/lib/index.css'
+// var Slider = require('react-rangeslider')
+class SortControl extends React.Component {
 
+ 
+  render() {
+   
+    return (
+ 
+  <div className="filterWrapper">
             <div id={"sortLabel"} className="ui basic big horizontal label">Sort By:</div> 
             <button
                 className="ui red button"
-                onClick={(event) => props.handleSort(event.target.innerText)}
+                onClick={(event) => this.props.handleSort(event.target.innerText)}
             >Price</button>
             <button
                 className="ui button"
-                onClick={(event) => props.handleSort(event.target.innerText)}
+                onClick={(event) => this.props.handleSort(event.target.innerText)}
             >Rating</button>
         </div>
     )
+  }
 }
+
 export default SortControl
